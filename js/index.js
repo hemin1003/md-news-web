6939b3985367b83f1a62287ad50ac81f6288c2cd
minbohe <hemin_it@163.com>
2018-09-07 17:14:12 +0800

    updae

--- js/index.js
+++ js/index.js
@@ -1,36 +1,87 @@
 $(function() {
 		function outSideFn() {
-			this.hostname = "http://47.93.246.85:8090/yfax-news-api/api/htt/getMainList?domain=news_baby"
-			this.adHostname = "http://182.92.82.188:8084/yfax-htt-api/api/htt/queryAdsOutsideConfig"
+			this.hostname = "http://news.ytoutiao.net/yfax-news-api/api/htt/getLikeList";
+			this.adHostname = "http://182.92.82.188:8084";
+			this.Url;
+			this.page = 1;
 		}
 		outSideFn.prototype = {
 			Init() {
-				this.LazyFn();
-				this.ajaxFn();
+				var that = this;
+				this.ajaxFn(1);
 				this.ajaxDomain();
+				this.ajaxAdFn();
+				// 滚动事件
+				$(window).scroll(function() {
+					var doc_height = $(document).height();
+        			var scroll_top = $(document).scrollTop(); 
+        			var window_height = $(window).height();
+        			if(scroll_top + window_height >= doc_height) {
+        				that.page++
+        				setTimeout(that.LazyFn,100); // 由于数据是api插入的，所以需要延迟加载
+        				that.ajaxFn(that.page);
+        			}
+					$(".footer").slideDown(500);
+				});
+				setTimeout(this.LazyFn,100); // 由于数据是api插入的，所以需要延迟加载
+
+				
+
 			},
+			// 站外app下载链接url
 			ajaxDomain() {
-				$.get(this.adHostname,function(res) {
+				var that = this;
+				$.get(that.adHostname+"/yfax-htt-api/api/htt/queryAdsOutsideConfig",function(res) {
 					if(res.code == 200) {
 						$(".go_download a").attr("href",res.data.outSideAppDownloadUrl);
+						that.Url = res.data.outSideAppDownloadUrl;
 					}else {
-						console.error(this.adHostname+"请求出错！");
+						console.error(that.adHostname+"请求出错！");
 					}
 				});
 			},
-			ajaxFn() {
+			// 广告位接口
+			ajaxAdFn() {
 				var that = this;
+				$.get(that.adHostname+"/yfax-htt-api/api/htt/queryAdsOutside",function(res) {
+					if(res.code == 200) {
+						console.log(res);
+						for(var i = 0, L = res.data.length; i < L; i++) {
+							switch(res.data[i].outsidePosition){
+								case 1:
+									// 置顶
+								  // console.log(res.data[i].outsidePosition);
+								  break;
+								case 5:
+									// 文顶广告
+									$(".title_ad").html('<img data-src="'+res.data[i].imgUrlOutside+'"/>');
+									$(".title_ad").parent().attr("href",res.data[i].url);
+								  break;
+								case 6:
+									// 文末
+									$(".article_ad").html('<img data-src="'+res.data[i].imgUrlOutside+'"/>');
+									$(".article_ad").parent().attr("href",res.data[i].url);
+							  		break;
+							}
+						}
+						
+					}else {
+						console.error(that.adHostname+"请求出错！");
+					}
+				});
+			},
+			ajaxFn(page) {
+				var that = this;
+				var t = $(".article h1").text();
 				var datas = {
-					curPage: 1
-				}
+					curPage: page,
+					title: t
+				};
 				$.ajax({
                     type:"get",
 					url: this.hostname,
 					data: datas,
-					// dataType:"jsonp",
-					// jsonp:"callback",
-          			// jsonpCallback:"success_jsonpCallback",
-					// async: true,
+					async: false,
                     success: function(res) {
                     	console.log(res);
 
@@ -39,8 +90,8 @@ $(function() {
                     			Url = res.data.entityList[i].url,
                     			category = res.data.entityList[i].category,
                     			Img = res.data.entityList[i].imageList[0];
-
-                    		$(".guss_title ul").after('<a href="'+Url+'"><li><div class="guss_font"><div class="guss_list_title">'+Title+'</div><div class="guss_list_source">'+category+'</div></div><img src="'+Img+'" alt="ads"></li></a>');
+                    		
+                    		$(".guss_like ul").append('<a href="'+that.Url+'"><li><div class="guss_font"><div class="guss_list_title">'+Title+'</div><div class="guss_list_source">'+category+'</div></div><img data-src="'+Img+'" alt="ads"></li></a>');
                     	}
                      },
                      error:function(res) {
@@ -50,10 +101,11 @@ $(function() {
                  });
 			},
 			LazyFn() {
-				$(".content img").lazyload({ 
+				// threshold: 100
+				$("img").lazyload({ 
 		          	placeholder : "images/loading.gif",
 		            effect: "fadeIn",
-		            data_attribute: "src"
+		            data_attribute: "src",
 		        }); 
 			},
 			go() {
