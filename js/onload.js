4949c61236f94c6625fa07a771c19e27032792a6
lafitewu <863512936@qq.com>
2018-11-14 11:11:43 +0800

    cdn 加速提交

--- js/onload.js
+++ js/onload.js
@@ -1,4 +1,4 @@
-    window.onload=function(){
-        setTimeout('$(".cover").hide()',300);
-    }
-    document.write('<div class="cover"><img src="./images/loading2.gif"></div>');
+window.onload=function(){
+    setTimeout('$(".cover").hide()',300);
+}
+document.write('<div class="cover"><img src="http://sidenewsstatic.ytoutiao.net/loading2.gif"></div>');
